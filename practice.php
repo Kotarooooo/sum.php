bf55f4861b17e86f981a114b5aa496f9fb0a5258
石田浩太郎 <wtrlndhr@gmail.com>
2020-01-28 11:49:16 +0000

    コミットのメッセージ

--- practice.php
+++ practice.php
@@ -1,36 +1,51 @@
 <?php
 echo 'hello php';
 echo "\n";
-$name = "Kotaro";
-if ($name =="Kota") {
-    echo "あなたの名前です。";
-} else {
-    echo "あなたの名前ではありません。";
-}
-echo"/n";
-  for ($i = 0; $i <= 10000; $i++) {
-      $total += $i;
-  }
-echo $total;
-echo"/n";
-$fruits = array("apple", "lemon", "orange", "banana", "cherry");
-foreach ($fruits as $fruit){
-    echo "フルーツは" .$fruit;
-echo"/n";
-}
-echo"/n";
-$start = 1;
-$end = 100;
 
-for($i = $start; $i < $end; $i++){
-    if($i / 5 == 0){
-        echo $i;
-    }
-    
-    
-    
-    
-}
+$a = 3;
+$b = 7;
+
+echo $a + $b;
+echo "\n";
+
+$array_month = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"];
+echo $array_month[7];
+
+echo "\n";
+
+$hello = "Hello";
+$name =  "Kotaro";
+$world = "'sWorld!";
+
+echo $hello,$name,$world;
+echo "\n";
+
+$tech_boost = "tech";
+$tech_boost .= "boost";
+
+echo $tech_boost;
+echo "\n";
+
+$calender = [
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月"
+];
+
+// 12月を表示する
+echo $calendar ["December"];
+
+
+
 
 
 
